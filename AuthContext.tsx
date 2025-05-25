b4b08dc28a1192b00385f94517054f5db5d3beca
KMB <kylematthewberry@gmail.com>
2025-05-25 01:51:56 -0400

    ok

--- AuthContext.tsx
+++ AuthContext.tsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+axios.defaults.baseURL = 'http://10.0.0.34:4000';
 import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
